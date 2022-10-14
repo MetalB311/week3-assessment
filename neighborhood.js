90b8ecf1298bef5b0ff334b6d3ed3d9be10288d5
MetalB <metalb311@gmail.com>
2022-10-14 12:50:02 -0600

    final push, couldnt get random button randomizing

--- neighborhood.js
+++ neighborhood.js
@@ -5,7 +5,9 @@ let rdmArr = ['SlapFish', 'StrapTank', 'R&R BBQ']
 
 function randomRestaurant(event) {
     // event.preventDefault()
-    alert(rdmArr)
+    alert((rdmArr))    //this one at least gives me the list LOL
+    // alert (rdmArr[Math.random() * array.length)  tried this
+    // alert(rdmArr.shuffle(list)); tried this one too
 }
 
 
